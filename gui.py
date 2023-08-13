87723998cc9c2c319519d1b1e688cad2e28f714b
JustAbhi77 <ojhaabhinav2016@gmail.com>
2023-08-13 20:15:25 +0530

    Overhauled code for readability.

--- gui.py
+++ gui.py
@@ -1,17 +1,14 @@
 from tkinter import *
 from helpers import *
 
+PLAYER = 1
+EMPTY = 0
+AI = -1
 
-class Gui(Tk):
-    """
-    Custom Tkinter selfdow class.
-    """
 
+class HexapawnGUI(Tk):
     def __init__(self, title: str):
-        """
-        Constructor for custom Settings.
-        """
-        Tk.__init__(self)
+        super().__init__()
         self.title(title)
         self.resizable(True, True)
         self.wpawn = PhotoImage(file='assets/white.gif')
@@ -19,100 +16,109 @@ class Gui(Tk):
         self.empty = PhotoImage(file='assets/empty.gif')
         self.tk.call('wm', 'iconphoto', self._w, self.bpawn)
         self.selectedPiece = None
+
+        self.create_score_board()
+
+        self.AI_move_directions = (
+            Position(1, 0), Position(1, -1), Position(1, 1))
+        self.Player_move_directions = (
+            Position(-1, 0), Position(-1, 1), Position(-1, -1))
+        self.create_game_board()
+        self.center_window()
+
+    def create_score_board(self):
+        entries = [
+            ("Player", "Ai"),
+            ("0", "0")
+        ]
+
         table = Frame(self)
         table.config(bg='black')
         table.pack(pady=10)
 
-        columnname = Entry(table, fg='white', font=(
-            'Arial', 16, 'bold'), justify='center')
-        columnname.configure({"background": "Black", })
-        columnname.grid(row=0, column=0)
-        columnname.insert(index=END, string='Player')
+        for row, values in enumerate(entries):
+            for col, text in enumerate(values):
+                columnname = Entry(table, fg='white', font=(
+                    'Arial', 16, 'bold'), justify='center')
+                columnname.configure({"background": "Black"})
+                columnname.grid(row=row, column=col)
+                columnname.insert(index=END, string=text)
 
-        columnname = Entry(table, fg='white', font=(
-            'Arial', 16, 'bold'), justify='center')
-        columnname.configure({"background": "Black", })
-        columnname.grid(row=0, column=1)
-        columnname.insert(index=END, string='Ai')
-
-        columnname = Entry(table, fg='white', font=(
-            'Arial', 16, 'bold'), justify='center')
-        columnname.configure({"background": "Black", })
-        columnname.grid(row=1, column=0)
-        columnname.insert(index=END, string='0')
-
-        columnname = Entry(table, fg='white', font=(
-            'Arial', 16, 'bold'), justify='center')
-        columnname.configure({"background": "Black", })
-        columnname.grid(row=1, column=1)
-        columnname.insert(index=END, string='0')
-
-        Button(self, text="Hello World", command=self.printHelp).pack(
+        Button(self, text="Hello World", command=self.print_help).pack(
             fill=BOTH, pady=10)
 
-        Gameboard = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]]
-        print(Gameboard)
+    def create_game_board(self):
+        self.Gameboard = [[AI, AI, AI], [
+            EMPTY, EMPTY, EMPTY], [PLAYER, PLAYER, PLAYER]]
+
         tile_frame = Frame(self)
         tile_frame.pack(padx=60, pady=(0, 60), expand=True)
 
-        # Create game board tiles
-        color = 'black'
-        i = 0
+        colors = ('black', 'white')
 
         for row in range(3):
             for col in range(3):
-                tile = myButton(tile_frame, text='{},{}'.format(row, col))
+                tile = HexapawnButton(
+                    tile_frame, text='{},{}'.format(row, col))
                 tile.config(
                     relief=FLAT,
-                    bg=color,
-                    fg='white' if color == 'black' else 'black',
-                    activebackground=color,
+                    bg=colors[(row + col) % 2],
+                    fg='white' if colors[(row + col) %
+                                         2] == 'black' else 'black',
+                    activebackground=colors[(row + col) % 2],
                     command=lambda row=row, col=col, tile=tile: self.player_selected(
                         row, col, tile),
                 )
                 tile.grid(column=col, row=row)
-                Gameboard.append(tile)
-                color = 'black' if color == 'white' else 'white'
-                i += 1
 
-                tile.group = Gameboard[row][col]
+                tile.group = self.Gameboard[row][col]
+                tile.position.x = row
+                tile.position.y = col
 
                 if row == 0:
                     tile.config(image=self.bpawn)
                     tile.image = self.bpawn
-                if row == 1:
-                    tile.config(image=self.empty)
-                    tile.image = self.empty
-                if row == 2:
+                elif row == 2:
                     tile.config(image=self.wpawn)
                     tile.image = self.wpawn
+                else:
+                    tile.config(image=self.empty)
+                    tile.image = self.empty
 
-        self.centerselfdow()
+    def player_selected(self, row: int, col: int, tile: HexapawnButton):
+        if self.selectedPiece is None:
+            if tile.group != EMPTY:
+                print(f'Selected piece at ({row}, {col})')
+                self.selectedPiece = tile
+        else:
+            if tile.group != self.selectedPiece.group:
 
-    def player_selected(self, row: int, col: int, tile: myButton):
+                if tile.position in (self.selectedPiece.position + direction for direction in self.Player_move_directions):
+                    print(
+                        f'Valid move for selected piece, move from {self.selectedPiece.position} to ({row}, {col})')
+                    if self.Gameboard[row][col] == -self.selectedPiece.group:
+                        print(f'Captured piece at {row}, {col}, Deselected')
 
-        if tile.group != 0 and self.selectedPiece == None:
-            print("selected piece at",row,col)
-            self.selectedPiece = tile
+                    tile.config(image=self.selectedPiece.image)
+                    tile.image = self.selectedPiece.image
+                    self.selectedPiece.config(image=self.empty)
+                    self.selectedPiece.image = self.empty
 
-        elif self.selectedPiece != None and tile.group != self.selectedPiece.group:
-            print('move selected piece to', row, col)
+                    tile.group = self.selectedPiece.group
+                    self.Gameboard[row][col] = tile.group
 
-            tile.config(image=self.selectedPiece.image)
-            tile.image = self.selectedPiece.image
-            self.selectedPiece.config(image=self.empty)
-            self.selectedPiece.image = self.empty
-
-            tile.group = self.selectedPiece.group
-
-            self.selectedPiece.group = 0
+                    self.Gameboard[self.selectedPiece.position.x][self.selectedPiece.position.y] = 0
+                    self.selectedPiece.group = 0
+                else:
+                    print(
+                        f'Invalid move for selected piece at {self.selectedPiece.position}, Deselected')
 
             self.selectedPiece = None
 
-    def printHelp(self):
+    def print_help(self):
         print('Hello World')
 
-    def centerselfdow(self):
+    def center_window(self):
         self.update_idletasks()
         width = self.winfo_width()
         frm_width = self.winfo_rootx() - self.winfo_x()
@@ -126,5 +132,5 @@ class Gui(Tk):
 
 
 if __name__ == "__main__":
-    selfdowobj = Gui(title="Hexapawn")
-    selfdowobj.mainloop()
+    hexapawn_obj = HexapawnGUI(title="Hexapawn")
+    hexapawn_obj.mainloop()
--- helpers.py
+++ helpers.py
@@ -1,6 +1,27 @@
-from tkinter import *
+from tkinter import Button
 
-class myButton(Button):
-    
-    group:int = 0
-    image:PhotoImage = None
+class Position:
+    def __init__(self, x: int = 0, y: int = 0):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Position(x, y)
+
+    def __eq__(self, other_position: object) -> bool:
+        if isinstance(other_position, Position):
+            return self.x == other_position.x and self.y == other_position.y
+        return False
+
+    def __str__(self) -> str:
+        return f"({self.x},{self.y})"
+
+
+class HexapawnButton(Button):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+        self.group = 0
+        self.position = Position()
